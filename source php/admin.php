7534b4d490ea5aa0d1731b5f102840c8ff3882f8
Tschley <thomas.keiser01@t-online.de>
2020-12-04 00:53:04 +0100

    Erstellen der login, registrieren, admin, shop und kein_account Dateien

---
+++ source php/admin.php
@@ -0,0 +1,1 @@
+<P> milo test</P>
---
+++ source php/registrieren.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pflaumen Pflamminger: Registrierung</title>
+    </head>
+    <body>
+        <form action="registrieren.php" method="post">
+            Ihre E-Mail: <br>
+            <input type="text" name="email" size="20" maxlength="30" />
+            <br>
+            Vorname: <br>
+            <input type="text" name="vorname" size="20" maxlength="30" />
+            <br>
+            Nachname: <br>
+            <input type="text" name="nachname" size="20" maxlength="30" />
+            <br>
+            Passwort vergeben: <br>
+            <input type="password" name="passwort" size="20" maxlength="30" />
+            <br>
+            Passwort wiederholen: <br>
+            <input type="password" name="passwort1" size="20" maxlength="30" />
+            <br>
+            <input type="submit" value="Jetzt registrieren" />
+        </form>
+    </body>
+</html>
