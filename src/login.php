c1be5f5d0f3cc6fe8d3a61469aaa05804b9c13f5
luispflamminger <luis.pflamminger@gmail.com>
2020-12-05 01:19:30 +0100

    Login und Registrierung fertiggestellt. Bootstrap styles getestet

--- src/login.php
+++ src/login.php
@@ -3,40 +3,62 @@
     <head>
         <title>Pflaumen Pflamminger: Die besten Pflaumen</title>
         <?php
-        if(!isset($_POST["email"])) {
+        if(isset($_POST["email"])) {
+            $con = mysqli_connect("","root");
+            mysqli_select_db($con,"pflaumenshop");
+            $sql = "SELECT * FROM benutzer WHERE email = '".$_POST["email"]."' ";
+            $res = mysqli_query($con, $sql);
+            $dsatz = mysqli_fetch_assoc($res);
+            if (mysqli_num_rows($res) == 0) {
+                $meldung = "Diese E-Mail existiert nicht. Bitte registrieren Sie sich <a href='registrieren.php'>hier</a>.";
+            } else if($_POST["passwort"] == $dsatz["password"]) {
+                //E-Mail und Passwort korrekt
+                if($dsatz["typ"] == "admin") {
+                    session_start();
+                    $_SESSION["login"] = "ok";
+                    $_SESSION["vorname"] = $dsatz["vorname"];
+                    $_SESSION["nachname"] = $dsatz["name"];
+                    $_SESSION["email"] = $dsatz["email"];
+                    $_SESSION["typ"] = $dsatz["typ"];
+                    header("Location: admin.php");
+                    mysqli_close($con);
+                    exit;
+                } else if ($dsatz["typ"] == "kunde") {
+                    session_start();
+                    $_SESSION["login"] = "ok";
+                    $_SESSION["vorname"] = $dsatz["vorname"];
+                    $_SESSION["nachname"] = $dsatz["nachname"];
+                    $_SESSION["email"] = $dsatz["email"];
+                    $_SESSION["typ"] = $dsatz["typ"];
+                    header("Location: shop.php?newUser=false");
+                    mysqli_close($con);
+                    exit;
+                } else {
+                    $meldung = "Unbekannter Fehler.<br>Bitte versuchen Sie es nochmal!";
+                    mysqli_close($con);
+                }
+            } else {
+                $meldung = "Das Passwort ist falsch!";
+                mysqli_close($con);
+            }
+        }
         ?>
     </head>
     <body>
         <form action="login.php" method="post">
         Ihre E-Mail: <br>
-        <input type="text" name="email" size="20" maxlength="30" />
+        <input type="text" name="email" size="20" maxlength="30" value="<?php if (isset($_POST["email"])) { echo $_POST["email"]; } ?>" />
         <br>
         Passwort: <br>
-        <input type="password" name="passwort" size="20" maxlength="30" />
+        <input type="password" name="passwort" size="20" maxlength="30" value="<?php if (isset($_POST["passwort"])) { echo $_POST["passwort"]; } ?>" />
         <br>
         <input type="submit" value="Einloggen"/>
         </form>
         <p> Neu bei Pflaumen Pflamminger?<br> Dann hier <a href="registrieren.php"> registrieren</a> </p>
+        <?php
+        if (isset($meldung)) {
+            echo "<div>$meldung</div>";
+        }       
+        ?>
     </body>
-    <?php
-        } else{
-            $con = mysqli_connect("","root");
-            mysqli_select_db($con,"pflaumenshop");
-            $sql = "SELECT typ FROM benutzer WHERE email = '".$_POST["email"]."' ";
-            $res = mysqli_query($con, $sql);
-            $dsatz = mysqli_fetch_assoc($res);
-            if($dsatz["typ"] == "admin") {
-                header("Location: admin.php");
-                exit;
-            }
-            else if ($dsatz["typ"] == "kunde") {
-                header("Location: shop.php");
-                exit;
-            }
-            else {
-                header("Location: kein_account.php");
-                exit;
-            }
-        }
-    ?>
 </html>
--- src/registrieren.php
+++ src/registrieren.php
@@ -3,55 +3,70 @@
     <head>
         <title>Pflaumen Pflamminger: Registrierung</title>
         <?php
-            if(!isset($_POST["email"])) {
-
+        if(isset($_POST["email"])) {
+            $pattern = "/.+\@[A-Za-z]+\.[A-Za-z]+$/";
+            if ($_POST["passwort"] != $_POST["passwort1"]) {
+                //Passwörter stimmen nicht überein
+                $meldung = "Passwörter stimmen nicht überein!";
+            } else if (!preg_match($pattern, $_POST["email"])) {
+                //* E-Mail falsch
+                $meldung = "Bitte geben Sie eine korrekte E-Mail Adresse an!";
+            } else if ($_POST["vorname"] == "" || $_POST["nachname"] == "") {
+                //* Name oder Vorname fehlt
+                $meldung = "Bitte geben Sie ihren Vor- und Nachnamen an!";
+            } else {
+                //Eingaben sind korrekt
+                $con = mysqli_connect("","root");
+                mysqli_select_db($con,"pflaumenshop");
+                $sql = "INSERT benutzer (typ, email, password, vorname, name)";
+                $sql .= " values ('kunde', '" . $_POST["email"] . "', '";
+                $sql .= $_POST["passwort"] . "', '" . $_POST["vorname"] . "', '";
+                $sql .= $_POST["nachname"] . "');";
+                $res = mysqli_query($con, $sql);
+                
+                if (preg_match("/^Duplicate entry/i", mysqli_error($con))) {
+                    //E-Mail existiert bereits
+                    $meldung = "Diese E-Mail existiert bereits! Bitte loggen Sie sich <a href='login.php'>hier</a> ein.";
+                    mysqli_close($con);
+                } else { //TODO: Datenbank auf positive Rückmeldung prüfen
+                    //Registrierung erfolgreich
+                    session_start();
+                    $_SESSION["login"] = "ok";
+                    $_SESSION["vorname"] = $_POST["vorname"];
+                    $_SESSION["nachname"] = $_POST["nachname"];
+                    $_SESSION["email"] = $_POST["email"];
+                    $_SESSION["typ"] = "kunde";
+                    header("Location: shop.php?newUser=true");
+                    exit;
+                }
+                
+            }
+        }
         ?>
     </head>
     <body>
         <form action="registrieren.php" method="post">
             Ihre E-Mail: <br>
-            <input type="text" name="email" size="20" maxlength="30" />
+            <input type="text" name="email" size="20" maxlength="30" value="<?php if (isset($_POST["email"])) { echo $_POST["email"]; } ?>" />
             <br>
             Vorname: <br>
-            <input type="text" name="vorname" size="20" maxlength="30" />
+            <input type="text" name="vorname" size="20" maxlength="30" value="<?php if (isset($_POST["vorname"])) { echo $_POST["vorname"]; } ?>"/>
             <br>
             Nachname: <br>
-            <input type="text" name="nachname" size="20" maxlength="30" />
+            <input type="text" name="nachname" size="20" maxlength="30" value="<?php if (isset($_POST["nachname"])) { echo $_POST["nachname"]; } ?>"/>
             <br>
             Passwort: <br>
-            <input type="password" name="passwort" size="20" maxlength="30" />
+            <input type="password" name="passwort" size="20" maxlength="30" value="<?php if (isset($_POST["passwort"])) { echo $_POST["passwort"]; } ?>"/>
             <br>
             Passwort wiederholen: <br>
-            <input type="password" name="passwort1" size="20" maxlength="30" />
+            <input type="password" name="passwort1" size="20" maxlength="30" value="<?php if (isset($_POST["passwort1"])) { echo $_POST["passwort1"]; } ?>"/>
             <br>
             <input type="submit" value="Jetzt registrieren" />
         </form>
         <?php
-                } else {
-
-                    $pattern = "/.+\@[A-Za-z]+\.[A-Za-z]+$/";
-                    if ($_POST["passwort"] != $_POST["passwort1"]) {
-                        //* Passwörter sind nicht gleich
-                    } else if (!preg_match($pattern, $_POST["email"])) {
-                        //* E-Mail falsch
-                    } else if ($_POST["vorname"] == "" || $_POST["nachname"] == "") {
-                        //* Name oder Vorname fehlt
-                    } else {
-                        //Eingaben sind korrekt
-                        $con = mysqli_connect("","root");
-                        mysqli_select_db($con,"pflaumenshop");
-                        $sql = "INSERT benutzer (typ, email, password, vorname, name)";
-                        $sql .= " values ('kunde', '" . $_POST["email"] . "', '";
-                        $sql .= $_POST["passwort"] . "', '" . $_POST["vorname"] . "', '";
-                        $sql .= $_POST["nachname"] . "');";
-                        $res = mysqli_query($con, $sql);
-                        
-                        if (preg_match("/^Duplicate entry/i", $con["error"])) {
-                            //E-Mail existiert bereits
-                        }
-                        mysqli_close($con);
-                    }
-                }
+        if (isset($meldung)) {
+            echo "<div>$meldung</div>";
+        }        
         ?>
     </body>
 </html>
