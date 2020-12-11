2c4e9328244d538ffa777c4009b1099eaf0563ee
luispflamminger <luis.pflamminger@gmail.com>
2020-12-11 23:51:27 +0100

    Bewertungen auf Artikelseite und Verfassen von Bew. hinzugefügt

--- src/shop/artikel.php
+++ src/shop/artikel.php
@@ -19,17 +19,21 @@
 
         $con = db_verbinden();
         $res = mysqli_query($con, $sql);
-        $dsatz = mysqli_fetch_assoc($res);
+        $dsatzArt = mysqli_fetch_assoc($res);
 
-        echo "<title>Pflaumen Pflamminger: " . $dsatz["name"] . "</title>";
+        $sql = "SELECT * FROM bewertung WHERE artikel=$id";
+
+        $res = mysqli_query($con, $sql);
+
+        echo "<title>Pflaumen Pflamminger: " . $dsatzArt["name"] . "</title>";
         ?>
 
         
     </head>
     <body>
-        <h2><?php echo $dsatz["name"]; ?></h2>
-        <img src='../../<?php echo $dsatz["bild"]; ?>' width='100' height='100' />
-        <p><?php echo $dsatz["beschreibung"]; ?></p>
+        <h2><?php echo $dsatzArt["name"]; ?></h2>
+        <img src='../../<?php echo $dsatzArt["bild"]; ?>' width='100' height='100' />
+        <p><?php echo $dsatzArt["beschreibung"]; ?></p>
 
         <form action="artikel.php" method="POST">
             <select name="anzahl" >
@@ -40,6 +44,21 @@
         </form>
 
         <h3>Bewertungen</h3>
+
+        <?php
+        while ($dsatzBew = mysqli_fetch_assoc($res)) {
+            $sql = "SELECT name FROM benutzer WHERE id=" . $dsatzBew["benutzer"];
+            $vornameBew = mysqli_fetch_assoc(mysqli_query($con, $sql));
+            echo "<p>--------------------------------------------------------------</p>";
+            echo "<p>Bewertung von " . $vornameBew["name"] . "</p>";
+            echo "<p>" . $dsatzBew["bewertung"] . " von 5 Pflaumen</p>";
+            echo "<h4>" . $dsatzBew["titel"] . "</h4>";
+            echo "<p>" . $dsatzBew["beschreibung"] . "</p>";
+            echo "<a href='bewertung.php?id=" . $dsatzBew["id"] . "'>Antworten anzeigen</a> | <a href='antw_verfassen.php?" . $dsatzBew["id"] . "'>Antwort verfassen</a>";
+        }
+        echo "<p>--------------------------------------------------------------</p>";
+        echo "<a href='bew_verfassen.php?id=" . $dsatzArt["id"] . "'>Bewertung verfassen</a>"; 
+        ?>
         <p><a href="start.php">Zur Startseite</a></p>
     </body>
 </html>
---
+++ src/shop/bew_verfassen.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pflaumen Pflamminger: Kategorien</title>
+    <link rel="stylesheet" href="../style.css" />
+    <?php
+    session_start();
+    require "../funktionen.php";
+    authentifizierung($_SESSION["typ"], "kunde");
+
+    if (isset($_GET["id"])) {
+        $artId = $_GET["id"];
+    }
+
+    if(!isset($_POST["verfassen"])) {
+        $meldung = "";
+    }
+    else {
+
+        if(!isset($_POST["titel"]) || $_POST["titel"] == "") {
+            $meldung = "Bitte einen Titel eingeben!";
+        } else if (!isset($_POST["beschreibung"]) || $_POST["beschreibung"] == "") {
+            $meldung = "Bitte eine Beschreibung eingeben!";
+        } else if (!isset($_POST["artId"])) {
+            $meldung = "Ein Fehler ist aufgetreten...";
+        } else {
+            
+            $artId = $_POST["artId"];
+
+            $con = db_verbinden();
+            $sql = "SELECT id FROM benutzer WHERE email = '" . $_SESSION["email"] . "'";
+            $res = mysqli_query($con, $sql);
+            $dsatz = mysqli_fetch_assoc($res);
+
+            $sql = "INSERT bewertung (titel, beschreibung, bewertung, benutzer, artikel) VALUES ('" . $_POST["titel"]
+                  . "', '" . $_POST["beschreibung"] . "', " . $_POST["bewertung"] . ", " . $dsatz["id"] . ", " . $artId . ");";
+
+            $res = mysqli_query($con, $sql);
+
+            if ($res && mysqli_affected_rows($con) == 1) {
+                $id = mysqli_insert_id($con);
+                
+                header("Location: bewertung.php?id=$id");
+            } else {
+                $meldung = "Etwas ist schiefgelaufen...";
+            }
+        }
+        
+
+    }
+    ?>
+</head>
+<body>
+    <h2>Bewertung verfassen</h2>
+    <form action = bew_verfassen.php method="post">
+        Titel: <input type="text" name="titel" size=20 maxlength="30"><br>
+        Beschreibung: <textarea rows="8" cols="50" name="beschreibung"></textarea><br>
+        Bewertung: 
+        1 <input type="radio" name="bewertung" value="1" />
+        2 <input type="radio" name="bewertung" value="2" />
+        3 <input type="radio" name="bewertung" value="3" />
+        4 <input type="radio" name="bewertung" value="4" />
+        5 <input type="radio" name="bewertung" value="5" checked />
+        <input type="hidden" name="artId" value="<?php echo $artId; ?>">
+        <input type="submit" name="verfassen" value="Verfassen">
+    </form>
+    <br>
+    <a href="artikel.php">Zurück</a>
+    <?php
+    echo "<p>$meldung</p>";
+    ?>
+</body>
+</html>
